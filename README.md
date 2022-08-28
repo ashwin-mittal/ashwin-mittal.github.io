8291e928ca3c8e1793c08ef11328708d423d284e
ashwin-mittal <ashwin.mittal2553@gmail.com>
2022-08-29 00:26:27 +0530

    Minor updates

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ### My Website :smiley:
+
 My website is written from scratch in HTML, CSS, javascript. The modularity of code is maintained.
 
 ---
+
 Made with :heart: by Ashwin Mittal
--- javascript/loader.js
+++ javascript/loader.js
@@ -1,9 +1,9 @@
 function load_page(name) {
-    const request = new XMLHttpRequest();
-    request.open('GET', `${name}.htm`);
-    request.onload = () => {
-        const response = request.responseText;
-        document.querySelector(`.${name}`).innerHTML = response;
-    };
-    request.send();
+  const request = new XMLHttpRequest();
+  request.open("GET", `${name}.htm`);
+  request.onload = () => {
+    const response = request.responseText;
+    document.querySelector(`.${name}`).innerHTML = response;
+  };
+  request.send();
 }
